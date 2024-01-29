f582e1216558595163f6f585a0ca8e700d1da828
Sang-minKIM <kimpran@naver.com>
2024-01-29 14:57:22 +0900

    feat: add submit-todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { TodoForm } from "./components/TodoForm";
 import { TodoList } from "./components/TodoList";
 import styles from "./app.module.scss";
+import { useState } from "react";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+
   return (
     <div className={styles.container}>
       <header className={styles.header}>My Todo App</header>
       <main className={styles.main}>
-        <TodoForm />
-        <TodoList />
+        <TodoForm setTodoList={setTodoList} />
+        <TodoList todoList={todoList} />
       </main>
     </div>
   );
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,10 +1,37 @@
+import { useState } from "react";
 import styles from "./todoForm.module.scss";
 
-export function TodoForm() {
+export function TodoForm({ setTodoList }) {
+  const [todoText, setTodoText] = useState("");
+
+  const handleInputChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+
+    setTodoText(value);
+  };
+
+  const submitTodo = () => {
+    setTodoText("");
+    if (!todoText.trim()) {
+      return;
+    }
+    setTodoList((prev) => [...prev, todoText]);
+  };
+
   return (
     <form action="" className={styles.todoForm}>
-      <input type="text" className={styles.todo} placeholder="할일을 입력하세요" />
-      <button className={styles.submit}>등록</button>
+      <input
+        type="text"
+        className={styles.todo}
+        placeholder="할일을 입력하세요"
+        onChange={handleInputChange}
+        value={todoText}
+      />
+      <button type="button" onClick={submitTodo} className={styles.submit}>
+        등록
+      </button>
     </form>
   );
 }
