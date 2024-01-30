1e9f199081381efa642635d0c2a7d8b8f00b07e9
Sang-minKIM <kimpran@naver.com>
2024-01-30 13:43:33 +0900

    refactor: make fetchTodo state enum

--- src/App.jsx
+++ src/App.jsx
@@ -2,44 +2,61 @@ import { TodoForm } from "./components/TodoForm";
 import { TodoList } from "./components/TodoList";
 import styles from "./app.module.scss";
 import { useEffect, useState } from "react";
-import { getTodoList } from "./service/service";
+
+const BASE_URL = "http://localhost:3000/todolist";
+
+const state = {
+  loading: "loading",
+  error: "error",
+  success: "success",
+};
 
 function App() {
   const [todoList, setTodoList] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const [promiseState, setPromiseState] = useState(state.loading);
+
+  const fetchTodo = () => {
+    setPromiseState(state.loading);
+    fetch(BASE_URL)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("GET 요청 실패");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setTodoList(data);
+        setPromiseState(state.success);
+      })
+      .catch((error) => {
+        setPromiseState(state.error);
+        console.log(error);
+      });
+  };
 
   useEffect(() => {
-    const fetchTodo = async () => {
-      try {
-        setTodoList(null);
-        setError(null);
-        setLoading(true);
-        const response = await getTodoList();
-        setTodoList(response);
-      } catch (error) {
-        setError(error);
-      }
-      setLoading(false);
-    };
     fetchTodo();
   }, []);
 
-  if (loading) return <div>로딩중..</div>;
-  if (error) return <div>에러가 발생했습니다</div>;
-  if (!todoList) return null;
-
-  return (
-    <div className={styles.container}>
-      <header className={styles.header}>
-        나<sub>만의</sub> 작<sub>은</sub> 스<sub>케줄러</sub>
-      </header>
-      <main className={styles.main}>
-        <TodoForm setTodoList={setTodoList} />
-        <TodoList todoList={todoList} setTodoList={setTodoList} />
-      </main>
-    </div>
-  );
+  if (promiseState == "loading") {
+    return <div>로딩중..</div>;
+  }
+  if (promiseState == "error") {
+    return <div>에러가 발생했습니다</div>;
+  }
+  if (promiseState == "success") {
+    return (
+      <div className={styles.container}>
+        <header className={styles.header}>
+          나<sub>만의</sub> 작<sub>은</sub> 스<sub>케줄러</sub>
+        </header>
+        <main className={styles.main}>
+          <TodoForm setTodoList={setTodoList} />
+          <TodoList todoList={todoList} setTodoList={setTodoList} />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
