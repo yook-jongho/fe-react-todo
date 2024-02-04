782adeff817b90d19024f9489d548a4885886c92
육종호 <dbrwhdgh1203@naver.com>
2024-02-04 13:35:33 +0900

    refactor: TodoList rendering using map function

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,37 +2,41 @@ import { useState } from "react";
 import styles from "./todoList.module.scss";
 
 export function TodoList({ todoList, setTodoList }) {
-  return (
-    <ul className={styles.todoList}>
-      {todoList.map((todoItem, idx) => (
-        <TodoItem key={idx} idx={idx} todoItem={todoItem} setTodoList={setTodoList} />
-      ))}
-    </ul>
-  );
+    const TodoListRender = todoList.map((item, index) => (
+        <TodoItem
+            key={index}
+            idx={index}
+            todoItem={item}
+            setTodoList={setTodoList}
+        ></TodoItem>
+    ));
+    return <ul className={styles.todoList}>{TodoListRender}</ul>;
 }
 
 function TodoItem({ todoItem: { text, complete }, setTodoList, idx }) {
-  const [isComplete, setIsComplete] = useState(complete);
+    const [isComplete, setIsComplete] = useState(complete);
 
-  const deleteTodo = () => {
-    setTodoList((prev) => prev.filter((_, index) => index !== idx));
-  };
+    const deleteTodo = () => {
+        setTodoList((prev) => prev.filter((_, index) => index !== idx));
+    };
 
-  const completeTodo = () => {
-    setIsComplete((prev) => !prev);
-  };
+    const completeTodo = () => {
+        setIsComplete((prev) => !prev);
+    };
 
-  return (
-    <li className={styles.todoItem}>
-      <p
-        className={`${styles.todoContent} ${isComplete ? styles.complete : ""}`}
-        onClick={completeTodo}
-      >
-        {text}
-      </p>
-      <button className={styles.deleteTodo} onClick={deleteTodo}>
-        삭제
-      </button>
-    </li>
-  );
+    return (
+        <li className={styles.todoItem}>
+            <p
+                className={`${styles.todoContent} ${
+                    isComplete ? styles.complete : ""
+                }`}
+                onClick={completeTodo}
+            >
+                {text}
+            </p>
+            <button className={styles.deleteTodo} onClick={deleteTodo}>
+                삭제
+            </button>
+        </li>
+    );
 }
