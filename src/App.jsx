3baae948acdffafcdf904bf3ae1a74bfc672f68f
육종호 <dbrwhdgh1203@naver.com>
2024-02-04 13:30:15 +0900

    feat: 새로운 todo 추가 및 리렌더링 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,30 @@ import { TodoForm } from "./components/TodoForm";
 import { TodoList } from "./components/TodoList";
 import styles from "./app.module.scss";
 import { useAsync } from "./hooks/useAsync";
-
-const BASE_URL = "http://localhost:3000/todolist";
-
-const getTodo = async () => {
-  const response = await fetch(BASE_URL);
-  return response;
-};
+import { getTodo } from "./service/service";
 
 function App() {
-  const [promiseState, refetchTodo] = useAsync(getTodo);
+    const [promiseState, refetchTodo] = useAsync(getTodo, []);
 
-  const { loading, error, data: todoList } = promiseState;
+    const { loading, error, data: todoList } = promiseState;
 
-  if (loading) {
-    return <div>로딩중..</div>;
-  }
-  if (error || !todoList) {
-    return <div>에러가 발생했습니다</div>;
-  }
-  return (
-    <div className={styles.container}>
-      <header className={styles.header}>
-        나<sub>만의</sub> 작<sub>은</sub> 스<sub>케줄러</sub>
-      </header>
-      <main className={styles.main}>
-        <TodoForm />
-        <TodoList todoList={todoList} />
-      </main>
-    </div>
-  );
+    if (loading) {
+        return <div>로딩중..</div>;
+    }
+    if (error || !todoList) {
+        return <div>에러가 발생했습니다</div>;
+    }
+    return (
+        <div className={styles.container}>
+            <header className={styles.header}>
+                나<sub>만의</sub> 작<sub>은</sub> 스<sub>케줄러</sub>
+            </header>
+            <main className={styles.main}>
+                <TodoForm todoList={refetchTodo} />
+                <TodoList todoList={todoList} />
+            </main>
+        </div>
+    );
 }
 
 export default App;
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,38 +1,45 @@
 import { useState } from "react";
 import styles from "./todoForm.module.scss";
+import { useAsync } from "../hooks/useAsync";
+import { postTodo } from "../service/service";
 
-export function TodoForm({ setTodoList }) {
-  const [todoText, setTodoText] = useState("");
+export function TodoForm(refetchTodo) {
+    const [todoText, setTodoText] = useState("");
+    const [state, fetchTodo] = useAsync(
+        () => postTodo({ text: todoText, complete: false }),
+        [todoText],
+        true
+    );
 
-  const handleInputChange = (event) => {
-    const {
-      target: { value },
-    } = event;
+    const handleInputChange = (event) => {
+        const {
+            target: { value },
+        } = event;
 
-    setTodoText(value);
-  };
+        setTodoText(value);
+    };
 
-  const submitTodo = (event) => {
-    event.preventDefault();
-    setTodoText("");
-    if (!todoText.trim()) {
-      return;
-    }
-    setTodoList((prev) => [...prev, todoText]);
-  };
+    const submitTodo = async (event) => {
+        event.preventDefault();
+        setTodoText("");
+        if (!todoText.trim()) {
+            return;
+        }
+        fetchTodo().then(refetchTodo.todoList);
+    };
 
-  return (
-    <form onSubmit={submitTodo} className={styles.todoForm}>
-      <input
-        type="text"
-        className={styles.todo}
-        placeholder="할일을 입력하세요"
-        onChange={handleInputChange}
-        value={todoText}
-      />
-      <button type="submit" className={styles.submit}>
-        등록
-      </button>
-    </form>
-  );
+    return (
+        <form onSubmit={submitTodo} className={styles.todoForm}>
+            <input
+                type="text"
+                className={styles.todo}
+                placeholder="할일을 입력하세요"
+                onChange={handleInputChange}
+                value={todoText}
+            />
+            <button type="submit" className={styles.submit}>
+                등록
+            </button>
+        </form>
+    );
 }
